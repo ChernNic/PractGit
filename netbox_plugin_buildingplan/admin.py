85789ad8d14f6fe2763ccfd60f6e823aa75bc80c
Николай Чернявский <75169213+ChernNic@users.noreply.github.com>
2024-10-26 01:49:03 +0300

    Update admin.py

--- netbox_plugin_buildingplan/admin.py
+++ netbox_plugin_buildingplan/admin.py
@@ -1,6 +1,1 @@
-from django.contrib import admin
-from .models import BuildingPlan
 
-@admin.register(BuildingPlan)
-class BuildingPlanAdmin(admin.ModelAdmin):
-    list_display = ['tenant', 'image']
